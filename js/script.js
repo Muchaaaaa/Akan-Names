3270f191f88f84bdad3db72f2b4cbe144fdffe6a
Muchaaaaa <sylviaslyg@gmail.com>
2021-07-17 23:46:33 +0300

    js functions added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-function onsubmit(){
-    
-}
+function onsubmit(birthday,gender){
+    var CC,YY,MM,YY.mod,d,DD;
+    var daysoftheweek=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    var maleNames=["Kwasi","Kwado","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+  }
+  
+  function getGender(){
+    var Gender=document.getElementByName("Gender").value);
+  
+  }
+    const d="(((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)mod 7)";
+  
+    console.log("Your Akan name is "+ Akan name)
+  
+  
+  let birthday=parseInt(Prompt("Enter your birthday": ))
+  let gender=parseInt(Prompt("Choose your gender": ))
