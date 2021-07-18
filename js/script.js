593e10f4d51ac44af7555e3af3a5d4a19ba0fab9
Muchaaaaa <sylviaslyg@gmail.com>
2021-07-18 19:39:01 +0300

    Js adjustments

--- js/script.js
+++ js/script.js
@@ -3,14 +3,13 @@ function onsubmit(birthday,gender){
     var daysoftheweek=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
     var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
     var maleNames=["Kwasi","Kwado","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    const d="(((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)mod 7)";
   }
   
   function getGender(){
     var Gender=document.getElementByName("Gender").value);
   
   }
-    const d="(((CC/4)-2*CC-1)+((5*YY/4))+((26*(MM+1)/10))+DD)mod 7)";
-  
     console.log("Your Akan name is "+ Akan name)
   
   
